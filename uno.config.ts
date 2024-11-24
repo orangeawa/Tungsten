79e25925374a5907ad986e8dbd3cfc333eee8e2c
Lurantis <1510033076@qq.com>
2024-11-24 22:40:53 +0800

    chore: add icon references

--- uno.config.ts
+++ uno.config.ts
@@ -3,6 +3,7 @@ import { defineConfig, presetIcons, presetUno } from 'unocss'
 export default defineConfig({
   presets: [
     presetUno(),
+    // https://icones.js.org/collection/mdi
     presetIcons({
       collections: {
         mdi: () => import('@iconify-json/mdi/icons.json').then(i => i.default),
