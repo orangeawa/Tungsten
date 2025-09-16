08b302d941b1e9d3bae1ac10cc84cdd53192e576
OCCG <orangeawa@outlook.com>
2025-09-16 12:44:27 +0800

    feat(style): :lipstick: Move global styles and edit Home page appearance

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -4,7 +4,7 @@ setSiteTitle()
 
 <template>
   <div>
-    <p class="rounded-lg bg-purple-50 p-2 text-center ring ring-purple-600">
+    <p class="rounded-lg bg-purple-50 dark:bg-purple-400/60 p-2 text-center ring ring-purple-600 dark:ring-purple-600/50">
       欢迎来到PatchyVideo的新页面！，很多内容还需要时间来建设，如果您有什么想法，欢迎加入
       <a href="https://jq.qq.com/?k=fOJYEJt1">我们的群组</a>
       来讨论！
